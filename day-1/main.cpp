7c7060b5320d7ae32914f70df217b560c6f5faa2
okoenig <oliver.koenig@dpdhl.com>
2023-06-27 08:23:42 +0200

    stye: Configure line length formatter

--- day-1/main.cpp
+++ day-1/main.cpp
@@ -1,38 +1,38 @@
-#include <fstream>
-#include <vector>
-#include <iostream>
+#include "include/elvetools.h"
 #include <algorithm>
-#include <string>
+#include <fstream>
+#include <iostream>
 #include <sstream>
-#include "elvetools.h"
+#include <string>
+#include <vector>
 
-int main(int argc, char const *argv[])
-{
-    // Open file
-    std::ifstream caloriesFile("calories.txt");
-    if (!caloriesFile.is_open())
-    {
-        std::cout << "Failed to open file `calories.txt`.";
-        return 1;
-    }
+int main(int argc, char const *argv[]) {
+  // Open file
+  std::ifstream caloriesFile("data/challenge.txt");
+  if (!caloriesFile.is_open()) {
+    std::cout << "Failed to open file.";
+    return 1;
+  }
 
-    // Convert buffer to string and close stream
-    std::vector<std::string> caloriesVector = trimVectorEnd(convertStreamToVector(caloriesFile));
+  // Convert buffer to string and close stream
+  std::vector<std::string> caloriesVector =
+      trimVectorEnd(convertStreamToVector(caloriesFile));
 
-    // Parse file into 2D mapping of calories per Elve
-    std::vector<std::vector<int>> caloriesPerElve = parseCalories(caloriesVector);
+  // Parse file into 2D mapping of calories per Elve
+  std::vector<std::vector<int>> caloriesPerElve = parseCalories(caloriesVector);
 
-    // Create summary of caloriesPerElve and find Elve with the most calories
-    std::vector<int> sumOfCaloriesPerElve = aggregateCalories(caloriesPerElve);
+  // Create summary of caloriesPerElve and find Elve with the most calories
+  std::vector<int> sumOfCaloriesPerElve = aggregateCalories(caloriesPerElve);
 
-    // Get index of Elve with most calories
-    int idxOfEleveWithMostCalories = std::distance(
-        sumOfCaloriesPerElve.begin(),
-        std::max_element(
-            sumOfCaloriesPerElve.begin(),
-            sumOfCaloriesPerElve.end()));
+  // Get index of Elve with most calories
+  int idxOfEleveWithMostCalories =
+      std::distance(sumOfCaloriesPerElve.begin(),
+                    std::max_element(sumOfCaloriesPerElve.begin(),
+                                     sumOfCaloriesPerElve.end()));
 
-    std::cout << printf("Elve number %d has the most calories (%d in total)\n", idxOfEleveWithMostCalories + 1, sumOfCaloriesPerElve[idxOfEleveWithMostCalories]);
+  std::cout << printf("Elve number %d has the most calories (%d in total)\n",
+                      idxOfEleveWithMostCalories + 1,
+                      sumOfCaloriesPerElve[idxOfEleveWithMostCalories]);
 
-    return 0;
+  return 0;
 }
